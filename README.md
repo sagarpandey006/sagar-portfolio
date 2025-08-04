fea667dc32cd3e868ef9afffeb6dbe9216629b4a
sagarpandey006 <officialsagar006@gmail.com>
2025-08-04 18:27:41 +0530

    Project section done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Project is Under Progress
 
-It's Navbar and Landing page is properly functional and completed
+It's Navbar, Landing, About and Project pages is properly functional and completed
+
+Now next is to create skill section then improve experience section then hackathon section then contact then footer
--- src/data/devProjects.tsx
+++ src/data/devProjects.tsx
@@ -13,46 +13,47 @@ interface DevProjects {
     techStack:string[],
 }
 
-const devProject:DevProjects[] = [
-    {
-        title: "Mind'Skoon",
-        subtitle : "Web Development",
-        year : "2025", 
-        imgURL :  ms, 
-        desc : "The official website I built for DASBVP, featuring event details, team details, upcoming event updates. The platform serves as a connecting point for the team, facilitating updates to all team members.", 
-        link : "https://dasbvp.onrender.com/",
-        gitlink : "https://github.com/ishikamanchanda30/DASBVP",
-        techStack: ['React','TypeScript','TailwindCSS'],
-    },
-    {
-        title: "Portfolio Site",
-        subtitle : "Web Development",
-        year : "2025",
-        imgURL : pt,
-        desc : "A comprehensive website to connect Startups and College Societies featuring Smart AI society-sponsor matching with real-time chat. Streamlining the process for societies to find sponsorships for their events.", 
-        link : "https://github.com/ishikamanchanda30/Sponsorship-Bridge.git",
-        gitlink : "https://github.com/ishikamanchanda30/Sponsorship-Bridge.git",
-        techStack: ['React','TypeScript','TailwindCSS','NodeJS', 'ExpressJS', 'MongoDB', 'Redux', 'Socket', 'Python'],
-    },
-    {
-        title: "Daily Task Management",
-        subtitle : "Web Development",
-        year : "2025",
-        imgURL : dt,
-        desc : "A responsive and user-friendly front-end prototype website for a telematics platform focused on long-haul trucking efficiency.", 
-        link : "https://github.com/ishikamanchanda30/SIH-Website-A",
-        gitlink : "https://github.com/ishikamanchanda30/SIH-Website-A",
-        techStack: ['HTML','CSS','Javascript'],
-    },
-    {
-        title: "Grow MY Therapy",
-        subtitle : "Web Development",
-        year : "2025",
-        imgURL : gmt,
-        desc : "A responsive and user-friendly front-end prototype website for a telematics platform focused on long-haul trucking efficiency.", 
-        link : "https://github.com/ishikamanchanda30/SIH-Website-A",
-        gitlink : "https://github.com/ishikamanchanda30/SIH-Website-A",
-        techStack: ['HTML','CSS','Javascript'],
-    },
-]
+const devProject: DevProjects[] = [
+  {
+    title: "Mind'Skoon",
+    subtitle: "Web Development",
+    year: "2025",
+    imgURL: ms,
+    desc: "A gamified mental wellness platform where users grow a virtual tree by completing mindfulness activities and journaling. Features include diary entries, wellness tracking, and a motivational garden-based UI.",
+    link: "https://sagarminor-1.onrender.com/",
+    gitlink: "https://github.com/sagarpandey006/sagarminor",
+    techStack: ['React', 'TailwindCSS', 'NodeJS', 'ExpressJS', 'MongoDB'],
+  },
+  {
+    title: "Portfolio Site",
+    subtitle: "Web Development",
+    year: "2025",
+    imgURL: pt,
+    desc: "My personal developer portfolio showcasing projects, skills, resume, and interactive sections with animations and theme toggling. Designed for recruiters and collaborators to explore my work.",
+    link: "https://sagarpandey006.vercel.app/",
+    gitlink: "https://github.com/sagarpandey006/sagar-portfolio",
+    techStack: ['React', 'TypeScript', 'TailwindCSS'],
+  },
+  {
+    title: "Daily Task Management",
+    subtitle: "Web Development",
+    year: "2025",
+    imgURL: dt,
+    desc: "A full-stack task management app with secure authentication, real-time CRUD operations, and responsive design. Includes CI/CD with Neon DB, server-side rendering, and full Lighthouse performance score.",
+    link: "https://daily-task-orpin.vercel.app/",
+    gitlink: "https://github.com/sagarpandey006/daily-task",
+    techStack: ['Next.js', 'PostgreSQL', 'Drizzle ORM', 'Neon DB', 'TailwindCSS',],
+  },
+  {
+    title: "Grow MY Therapy",
+    subtitle: "Web Development",
+    year: "2025",
+    imgURL: gmt,
+    desc: "A clean and mobile-friendly website concept for a therapy platform. Highlights therapy services and guides users through growth, mindfulness, and self-help resources in an engaging layout.",
+    link: "https://grow-my-therapy-ten.vercel.app/",
+    gitlink: "https://github.com/sagarpandey006/Grow-my-therapy",
+    techStack: ['Next.js', 'React', 'TailwindCSS', 'JavaScript'],
+  }
+];
+
 export default devProject; 
